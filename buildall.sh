8ceea5954cfa49c5b676e77586b54969828ed914
Bin Cheng <showersky@gmail.com>
2018-04-28 22:29:35 +0200

    add the script to build all fogflow docker images

---
+++ buildall.sh
@@ -0,0 +1,16 @@
+cd discovery
+./build
+
+cd ../broker
+./build
+
+cd ../master
+./build
+
+cd ../worker
+./build
+
+cd ../
+echo "finished building all FogFlow core components and generating their docker images" 
+
+
