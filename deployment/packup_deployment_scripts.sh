d8f559fca63d802f0b4d8eb990fc404608e84fb7
Bin Cheng <showersky@gmail.com>
2018-06-01 18:00:55 +0200

    simplify the deployment scripts

--- deployment/packup_deployment_scripts.sh
+++
@@ -1,6 +0,0 @@
-rm  ./core/fogflow-core.tar.gz 
-tar -cf ./core/fogflow-core.tar.gz  ./core/*.json ./core/*.yml
-
-rm ./edge/fogflow-edge.tar.gz 
-tar -cf ./edge/fogflow-edge.tar.gz  ./edge/*.json ./edge/*.yml
-
