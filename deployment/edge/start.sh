e93e770a45cb46c8c191dbf39a16a8c7363b0636
Bin Cheng <showersky@gmail.com>
2018-06-04 15:34:58 +0200

    make this script for both arm based and non-arm based

--- deployment/edge/start.sh
+++ deployment/edge/start.sh
@@ -1,5 +1,9 @@
-dir=$(pwd)
+if [ $# -eq 0 ]; then
+	htype='latest'
+else
+	htype='arm'
+fi
 
-docker run -d --name=broker -v $(pwd)/config.json:/config.json -p 8080  fogflow/broker:latest
-docker run -d --name=worker -v $(pwd)/config.json:/config.json -p 8080  fogflow/worker:latest
+docker run -d --name=broker -v $(pwd)/config.json:/config.json -p 8080  fogflow/broker:$htype
+docker run -d --name=worker -v $(pwd)/config.json:/config.json -p 8080  fogflow/worker:$htype
 
