896ea35b31f35de049bec3725a51813b13aae4c1
smartfog <bin.cheng@neclab.eu>
2018-06-14 14:48:40 +0200

    Update start.sh

--- deployment/edge/start.sh
+++ deployment/edge/start.sh
@@ -5,5 +5,5 @@ else
 fi
 
 docker run -d --name=broker -v $(pwd)/config.json:/config.json -p 8080  fogflow/broker:$htype
-docker run -d --name=worker -v $(pwd)/config.json:/config.json -p 8080  fogflow/worker:$htype
+docker run -d --name=worker -v $(pwd)/config.json:/config.json fogflow/worker:$htype
 
