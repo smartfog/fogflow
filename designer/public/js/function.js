91b85aa3cdfb0323b76050b25f7a183bd41c25a6
Bin Cheng <bin.cheng@neclab.eu>
2018-07-24 19:06:52 +0200

    use internal ip and external ip for the system deployment

--- designer/public/js/function.js
+++ designer/public/js/function.js
@@ -6,43 +6,13 @@ var CurrentScene = null;
 
 var template = {};
 
-template.javascript = ' \
-    exports.handler = function(contextEntity, publish, query, subscribe) { \
-        console.log("enter into the user-defined fog function");    \
-        var entityID = contextEntity.entityId.id;    \
-                                                     \
-        if (contextEntity == null) {                 \
-            return;                                  \
-        }                                            \
-        if (contextEntity.attributes == null) {           \
-            return;                                        \
-        }                                            \
-                                                                \
-        var updateEntity = {};                                        \
-        updateEntity.entityId = {                    \
-            id: "Stream.result." + entityID,    \
-            type: 'result',    \
-            isPattern: false    \
-        };    \
-        updateEntity.attributes = {};\
-        updateEntity.attributes.city = {    \
-            type: 'string',    \
-            value: 'Heidelberg'    \
-        };    \
-                        \
-        updateEntity.metadata = {};    \
-        updateEntity.metadata.location = {    \
-            type: 'point',    \
-            value: {            \
-                'latitude': 33.0,    \
-                'longitude': -1.0    \
-            }                        \
-        };                        \
-                                    \
-        console.log("publish: ", updateEntity);    \
-        publish(updateEntity);    \
-    };    \
-';
+
+$.ajax({url:'js/function_template.js', 
+    success: function(fileContent){
+        console.log(fileContent);
+        template.javascript =  fileContent;      
+    }
+});
 
 template.python = 'define handler(context): \r\n \t # write your own code to do data processing  \r\n \t # return the generated context entities to be published as outputs';
 
