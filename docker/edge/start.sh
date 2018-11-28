b20fb76f73c6c378ddad91c4bc695b94498eeef5
smartfog <bin.cheng@neclab.eu>
2018-11-28 11:13:23 +0100

    Update start.sh

--- docker/edge/start.sh
+++ docker/edge/start.sh
@@ -4,6 +4,6 @@ else
 	htype='arm'
 fi
 
-docker run -d --name=edgebroker -v $(pwd)/config.json:/config.json -p 80:80  fogflow/broker:$htype
+docker run -d --name=edgebroker -v $(pwd)/config.json:/config.json -p 8080:8080  fogflow/broker:$htype
 docker run -d --name=edgeworker -v $(pwd)/config.json:/config.json -v /tmp:/tmp -v /var/run/docker.sock:/var/run/docker.sock fogflow/worker:$htype
 
