cd70d798c841b78f8ee041c36c60ac63995d77ea
Bin Cheng <showersky@gmail.com>
2019-01-07 21:48:30 +0100

    Create fetch_images.sh
    prefetch the docker images required by demo applications

---
+++ fetch_images.sh
@@ -0,0 +1,8 @@
+docker pull fogflow/dummy                                         
+docker pull fogflow/privatesite                                            
+docker pull fogflow/publicsite                              
+docker pull fogflow/connectedcar   
+docker pull fogflow/recommender      
+docker pull fogflow/counter                                        
+docker pull fogflow/anomaly                                            
+docker pull fogflow/facefinder                                          
