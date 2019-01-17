46de4e617bf0250d323dae0b1f6ba8d0f68205db
showersky <showersky@gmail.com>
2019-01-17 12:40:13 +0000

    fixed the problem with task assignment

--- master/master.go
+++ master/master.go
@@ -545,7 +545,10 @@ func (master *Master) SelectWorker(locations []Point) string {
 
 		totalDistance := uint64(0)
 
-		for _, location := range locations {
+		for _, location := range locations { 
+			if location.Latitude == 0 && location.Longitude == 0 {
+				continue
+			}
 			distance := Distance(wp, location)
 			totalDistance += distance
 			INFO.Printf("distance = %d between %+v, %+v\r\n", distance, wp, location)
