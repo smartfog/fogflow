a6e8f85e1045abf41bf0ca91cd942654b5949c2a
Amir <rahafrouz@gmail.com>
2019-07-29 23:41:21 +0200

    update build.sh
    The customized version of promethus docker container is created, and is used by the fogflow framework.

--- prometheus/build.sh
+++ prometheus/build.sh
@@ -2,7 +2,7 @@
 
 go get
 
-#build the linux version (amd64) of master
-env GOOS=linux GOARCH=amd64 go build  -a  -o PrometheusConfigUpdaterAPI
-docker build -t "rahafrouz/fogflow-prom" .
+#build the linux version (amd64) of prometheus for fogflow
 
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build  -a -installsuffix cgo  -o PrometheusConfigUpdaterAPI
+docker build -t "fogflow/prometheus" .
