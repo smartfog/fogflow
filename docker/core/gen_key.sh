fdcc2463e8b23e6eda876327977813c46de0ae0a
Bin Cheng <showersky@gmail.com>
2019-08-12 12:37:11 +0200

    add the script to generate the key and certificate

---
+++ docker/core/gen_key.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+BOLD=$(tput bold)
+CLEAR=$(tput sgr0)
+
+
+# for root CA
+
+echo -e "${BOLD}Generating RSA AES-256 Private Key for Root Certificate Authority${CLEAR}"
+openssl genrsa  -out root_ca.key 4096
+
+echo -e "${BOLD}Generating Certificate for Root Certificate Authority${CLEAR}"
+openssl req -x509 -new -nodes -key root_ca.key -days 1825 -out root_ca.pem -subj "/C=DE/ST=BW/L=Heidelberg/O=NEC/OU=NLE/CN=$1"
+
+
+# for cloud-node
+
+echo -e "${BOLD}Generating RSA Private Key for Server Certificate${CLEAR}"
+openssl genrsa -out cloud_node.key 4096
+
+echo -e "${BOLD}Generating Certificate Signing Request for Server Certificate${CLEAR}"
+openssl req -new -key cloud_node.key -out cloud_node.csr -subj "/C=DE/ST=BW/L=Heidelberg/O=NEC/OU=NLE/CN=$1"  
+
+echo -e "${BOLD}Generating Certificate for Server Certificate${CLEAR}"
+openssl x509 -req -in cloud_node.csr -CA root_ca.pem -CAkey root_ca.key -CAcreateserial -out cloud_node.pem -outform PEM -days 1825 
+
+
+# for edge-node
+
+echo -e "${BOLD}Generating RSA Private Key for Client Certificate${CLEAR}"
+openssl genrsa -out edge_node.key 4096
+
+echo -e "${BOLD}Generating Certificate Signing Request for Client Certificate${CLEAR}"
+openssl req -new -key edge_node.key -out edge_node.csr -subj "/C=DE/ST=BW/L=Heidelberg/O=NEC/OU=NLE/CN=$1"
+
+echo -e "${BOLD}Generating Certificate for Client Certificate${CLEAR}"
+openssl x509 -req -in edge_node.csr -CA root_ca.pem -CAkey root_ca.key -CAcreateserial -out edge_node.pem -outform PEM -days 1825 
+
+# for designer
+
+echo -e "${BOLD}Generating RSA Private Key for Client Certificate${CLEAR}"
+openssl genrsa -out designer.key 4096
+
+echo -e "${BOLD}Generating Certificate Signing Request for Client Certificate${CLEAR}"
+openssl req -new -key designer.key -out designer.csr -subj "/C=DE/ST=BW/L=Heidelberg/O=NEC/OU=NLE/CN=$2"
+
+echo -e "${BOLD}Generating Certificate for Client Certificate${CLEAR}"
+openssl x509 -req -in designer.csr -CA root_ca.pem -CAkey root_ca.key -CAcreateserial -out designer.pem -outform PEM -days 1825 
+
+
+cp cloud_node.pem cloud_node.key root_ca.pem  ../master
+
+cp edge_node.pem edge_node.key root_ca.pem  ../broker
+cp edge_node.pem edge_node.key root_ca.pem  ../worker
+
+cp designer.pem designer.key root_ca.pem  ../designer
+
+echo "Done!"
+
