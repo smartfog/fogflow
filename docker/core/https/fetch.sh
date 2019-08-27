2f326ac5eefa7c62daed769c69af32e216b54e8e
Bin Cheng <showersky@gmail.com>
2019-08-27 12:21:31 +0200

    add the script to download the setup files

---
+++ docker/core/https/fetch.sh
@@ -0,0 +1,11 @@
+wget https://raw.githubusercontent.com/smartfog/fogflow/master/docker/core/https/docker-compose.yml
+wget https://raw.githubusercontent.com/smartfog/fogflow/master/docker/core/https/config.json
+wget https://raw.githubusercontent.com/smartfog/fogflow/master/docker/core/https/nginx.conf
+
+wget https://raw.githubusercontent.com/smartfog/fogflow/master/docker/core/https/key4cloudnode.sh
+wget https://raw.githubusercontent.com/smartfog/fogflow/master/docker/core/https/key4edgenode.sh
+
+wget https://raw.githubusercontent.com/smartfog/fogflow/master/docker/core/https/bind/docker-compose.yml
+
+
+
