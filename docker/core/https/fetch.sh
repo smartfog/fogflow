2d73127591f56b5858950468ce7c056dae5b9e0c
Bin Cheng <showersky@gmail.com>
2019-08-28 15:09:01 +0000

    prepare the deployment script for https-based setup on the cloud node

--- docker/core/https/fetch.sh
+++ docker/core/https/fetch.sh
@@ -1,11 +1,9 @@
 wget https://raw.githubusercontent.com/smartfog/fogflow/master/docker/core/https/docker-compose.yml
 wget https://raw.githubusercontent.com/smartfog/fogflow/master/docker/core/https/config.json
 wget https://raw.githubusercontent.com/smartfog/fogflow/master/docker/core/https/nginx.conf
-
 wget https://raw.githubusercontent.com/smartfog/fogflow/master/docker/core/https/key4cloudnode.sh
-wget https://raw.githubusercontent.com/smartfog/fogflow/master/docker/core/https/key4edgenode.sh
-
-wget https://raw.githubusercontent.com/smartfog/fogflow/master/docker/core/https/bind.tar.gz
+wget https://raw.githubusercontent.com/smartfog/fogflow/master/docker/core/https/root_ca.pem
+wget https://raw.githubusercontent.com/smartfog/fogflow/master/docker/core/https/root_ca.key
 
 
 
