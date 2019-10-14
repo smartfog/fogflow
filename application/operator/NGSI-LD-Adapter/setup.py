421fb872b4e79d833e34a7a71ed4b950346a6277
tejinderkhattra <tejinderkhattra@india.nec.com>
2019-10-14 05:53:31 +0000

    Removed ngsi-ld operator.

--- application/operator/NGSI-LD-Adapter/setup.py
+++
@@ -1,6 +0,0 @@
-import sys, os
-testdir = os.path.dirname(__file__)
-srcdir = '/opt/ngsildAdapter/module'
-sys.path.insert(0, os.path.abspath(os.path.join(testdir, srcdir)))
-#from data_model.ld_generate  import ngsi_data_creation
-sys.path.append('/opt/ngsildAdapter/module')
--- application/operator/NGSI-LD-Adapter/setup.sh
+++
@@ -1,2 +0,0 @@
-export PYTHONPATH="$PYTHONPATH:/opt/ngsildAdapter/module"
-
