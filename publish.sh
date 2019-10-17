60fe2f5e19d7f21da35da1070cc0c66a76b79572
Bin Cheng <showersky@gmail.com>
2019-10-17 16:28:58 +0200

    add the lego demo

--- publish.sh
+++ publish.sh
@@ -1,14 +1,17 @@
 docker push  fogflow/discovery
+docker push  fogflow/discovery:arm
 
 docker push  fogflow/broker
 docker push  fogflow/broker:arm
 
 docker push  fogflow/master
+docker push  fogflow/master:arm
 
 docker push  fogflow/worker
 docker push  fogflow/worker:arm
 
 docker push  fogflow/designer
+docker push  fogflow/designer:arm
 
 
 
