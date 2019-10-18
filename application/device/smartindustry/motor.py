f725554cdec17a8c4fe836ba97be0f4eda2ab415
Bin Cheng <showersky@gmail.com>
2019-10-18 17:58:18 +0100

    add the device modules for the lego demo

--- application/device/smartindustry/motor.py
+++ application/device/smartindustry/motor.py
@@ -2,11 +2,16 @@ import nxt
 import sys
 import time
 
+#import nxt.locator
+#from nxt.motor import *
 
+#
 #b = nxt.locator.find_one_brick()
 b = nxt.find_one_brick()
 mx = nxt.Motor(b, nxt.PORT_A)
-mx.run(int(sys.argv[1]))
-time.sleep(int(sys.argv[2]))
+mx.run(100)
+time.sleep(2)
 mx.brake()
+
+print("finished")
 
