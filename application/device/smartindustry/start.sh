57f252ef945e52a25f6b47bfb164a2a653cae52a
Bin Cheng <showersky@gmail.com>
2019-10-19 21:36:44 +0200

    debug the motor

--- application/device/smartindustry/start.sh
+++ application/device/smartindustry/start.sh
@@ -10,7 +10,7 @@ echo "start motor"
 sudo python motor.py &
 
 echo "start tpu-based controller"
-docker run -d --privileged -p 25:22 -p 8000:8000 -p 8888:8888 --restart unless-stopped -v /dev/bus/usb:/dev/bus/usb --env-file env.list fogflow/tpu
+docker run -d --privileged -p 25:22 -p 8000:8000 -p 8888:8888  -v /dev/bus/usb:/dev/bus/usb --env-file env.list fogflow/tpu
 
 echo "already set up everything"
 
