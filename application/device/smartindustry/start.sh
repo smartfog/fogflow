91f4cef2690dd2c2bdff2f551f80920ccf55cdd7
Bin Cheng <showersky@gmail.com>
2019-10-21 15:08:55 +0200

    done with the lego demo

--- application/device/smartindustry/start.sh
+++ application/device/smartindustry/start.sh
@@ -1,11 +1,15 @@
+cd /home/pi/go/src/github.com/smartfog/fogflow/application/device/smartindustry 
+
 echo "start pushbutton"
-sudo  python /home/pi/go/src/github.com/smartfog/fogflow/application/device/smartindustry/button.py & > /home/pi/button.log 2>&1
+#sudo  python /home/pi/go/src/github.com/smartfog/fogflow/application/device/smartindustry/button.py & > /home/pi/button.log 2>&1
+sudo  python button.py > /tmp/button.log &
 
 echo "start camera"
-sudo  python /home/pi/go/src/github.com/smartfog/fogflow/application/device/smartindustry/camera.py & > /home/pi/camera.log 2>&1
+#sudo  python /home/pi/go/src/github.com/smartfog/fogflow/application/device/smartindustry/camera.py &
+sudo  python camera.py > /tmp/camera.log &
 
 echo "start motor"
-sudo  python /home/pi/go/src/github.com/smartfog/fogflow/application/device/smartindustry/motor.py & > /home/pi/motor.log 2>&1
+#sudo  python /home/pi/go/src/github.com/smartfog/fogflow/application/device/smartindustry/motor.py & > /home/pi/motor.log 2>&1
+sudo  python motor.py > /tmp/motor.log &
 
 echo "already set up everything"
-
