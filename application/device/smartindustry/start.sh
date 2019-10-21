60da69b7308c0f50793fb5425792f7eb216a6496
Bin Cheng <showersky@gmail.com>
2019-10-21 13:17:15 +0200

    make this start.sh to be scheduled by crontab

--- application/device/smartindustry/start.sh
+++ application/device/smartindustry/start.sh
@@ -1,13 +1,11 @@
-sleep 10
-
 echo "start pushbutton"
-python button.py & 
+sudo  python /home/pi/go/src/github.com/smartfog/fogflow/application/device/smartindustry/button.py & > /home/pi/button.log 2>&1
 
 echo "start camera"
-python camera.py & 
+sudo  python /home/pi/go/src/github.com/smartfog/fogflow/application/device/smartindustry/camera.py & > /home/pi/camera.log 2>&1
 
 echo "start motor"
-sudo python motor.py &
+sudo  python /home/pi/go/src/github.com/smartfog/fogflow/application/device/smartindustry/motor.py & > /home/pi/motor.log 2>&1
 
 echo "already set up everything"
 
