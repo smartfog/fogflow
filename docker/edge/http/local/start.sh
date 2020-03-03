e8667f729dedf83717b9afb17054e9d5b3cf2c9f
sanchitanec <sanchita.gupta@india.nec.com>
2020-03-03 05:42:23 +0000

    commit elasticsearch for system monitoring

--- docker/edge/http/local/start.sh
+++ docker/edge/http/local/start.sh
@@ -4,6 +4,7 @@ else
 	htype='arm'
 fi
 
+docker run -d   --name=metricbeat   --user=root   --volume="$(pwd)/metricbeat.docker.yml:/usr/share/metricbeat/metricbeat.yml:ro"   --volume="/var/run/docker.sock:/var/run/docker.sock:ro"   --volume="/sys/fs/cgroup:/hostfs/sys/fs/cgroup:ro"   --volume="/proc:/hostfs/proc:ro"   --volume="/:/hostfs:ro"   docker.elastic.co/beats/metricbeat:7.6.0 metricbeat -e   -E output.elasticsearch.hosts=["<ELASTICSEARCH_HOST_IP>:<ELASTICSEARCH_HOST_PORT>"]
 docker run -d --name=edgebroker -v $(pwd)/config.json:/config.json -p 8170:8170  fogflow/broker:$htype
 docker run -d --name=edgeworker -v $(pwd)/config.json:/config.json -v /tmp:/tmp -v /var/run/docker.sock:/var/run/docker.sock fogflow/worker:$htype
 
--- docker/edge/http/local/stop.sh
+++ docker/edge/http/local/stop.sh
@@ -1,3 +1,4 @@
+docker stop metricbeat && docker rm $_
 docker stop edgebroker && docker rm $_
 docker stop edgeworker && docker rm $_
 
