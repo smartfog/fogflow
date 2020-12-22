983830a6bd9253c5a71273bc8368fc74a653fb5e
pradumnnec <t_pradumn@india.nec.com>
2020-12-22 11:19:21 +0000

    Edge_Security: comment removal

--- docker/edge/http/start.sh
+++ docker/edge/http/start.sh
@@ -7,7 +7,7 @@ fi
 sh $(pwd)/script.sh
 if [ $? -eq 0 ]; then
     docker run -d   --name=metricbeat   --user=root   --volume="$(pwd)/metricbeat.docker.yml:/usr/share/metricbeat/metricbeat.yml:ro"   --volume="/var/run/docker.sock:/var/run/docker.sock:ro"   --volume="/sys/fs/cgroup:/hostfs/sys/fs/cgroup:ro"   --volume="/proc:/hostfs/proc:ro"   --volume="/:/hostfs:ro"   docker.elastic.co/beats/metricbeat:7.6.0 metricbeat -e   -E output.elasticsearch.hosts=["<Cloud_Public_IP>:9200"]
-    docker run -d --name=edgebroker -v $(pwd)/config.json:/config.json -p 8060:8060  fogflow/broker_edge
+    docker run -d --name=edgebroker -v $(pwd)/config.json:/config.json -p 8060:8060  fogflow/broker:3.2
     docker run -d --name=edgeworker -v $(pwd)/config.json:/config.json -v /tmp:/tmp -v /var/run/docker.sock:/var/run/docker.sock fogflow/worker:$htype
     docker run -d --name=pepEdge -v $(pwd)/pep-config.js:/opt/fiware-pep-proxy/config.js -p 5556:5556 fiware/pep-proxy
     #sh $(pwd)/user_addition.sh
--- docker/edge/http/stop.sh
+++ docker/edge/http/stop.sh
@@ -1,14 +1,7 @@
-#sh $(pwd)/delete.sh
-#if [ $? -eq 0 ]; then
-    docker stop metricbeat && docker rm $_
-    docker stop edgebroker && docker rm $_
-    docker stop edgeworker && docker rm $_
-    docker stop pepEdge && docker rm $_
-    #docker stop Nginx && docker rm $_
-    #echo "Deleting Edge Users"
-    #rm $(pwd)/user
-#else
-     #echo failed in delete Application
-#fi
+docker stop metricbeat && docker rm $_
+docker stop edgebroker && docker rm $_
+docker stop edgeworker && docker rm $_
+docker stop pepEdge && docker rm $_
+
 
 
