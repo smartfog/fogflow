3edd01369354909662910dca6d34177f4fe1ec32
pradumnnec <t_pradumn@india.nec.com>
2020-12-29 13:11:59 +0000

    Edge_Security: added comment in stop.sh

--- docker/edge/http/stop.sh
+++ docker/edge/http/stop.sh
@@ -1,3 +1,4 @@
+#SCript to bring down the edge and remove corresponding docker containers
 docker stop metricbeat && docker rm $_
 docker stop edgebroker && docker rm $_
 docker stop edgeworker && docker rm $_
