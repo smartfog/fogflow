b479328eb163bb8b305a87dbda84ecc633b6b828
root <neeraj.srivastava@india.nec.com>
2021-01-28 16:46:27 +0000

    add updated jmeter script

---
+++ test/NGSILD/subscription-latency/ngsildClient.py
@@ -0,0 +1,33 @@
+import requests
+import json
+
+
+# request that send the subscribe Context request
+
+def subscribeContext(subscribeCtxEle, BrokerURL):
+    headers = {'Accept': 'application/ld+json',
+               'Content-Type': 'application/json',
+               'Link': '<https://fiware.github.io/data-models/context.jsonld>; rel="https://uri.etsi.org/ngsi-ld/v1/ngsi-ld-core-context.jsonld"; type="application/ld+json"'}
+    response = requests.post(BrokerURL + '/ngsi-ld/v1/subscriptions/',
+                             data=json.dumps(subscribeCtxEle),
+                             headers=headers)
+    if response.status_code == 201:
+        return response.status_code
+    else:
+        return ''
+
+
+# request that send the update Context request to Broker
+def updateCotext(updateCtxEle, BrokerURL):
+    headers = {'Accept': 'application/ld+json',
+               'Content-Type': 'application/json',
+               'Link': '<https://fiware.github.io/data-models/context.jsonld>; rel="https://uri.etsi.org/ngsi-ld/v1/ngsi-ld-core-context.jsonld"; type="application/ld+json"'}
+    response = requests.post(scorpioBrokerURL + '/ngsi-ld/v1/entities/',
+                             data=json.dumps(updateCtxEle),
+                             headers=headers)
+
+    if response.status_code == 201:
+        return response.status_code
+    else:
+        return ''
+
