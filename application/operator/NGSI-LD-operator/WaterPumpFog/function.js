9a53a9cb282fa24be0f853535b8c0a9a7228abbd
root <neeraj.srivastava@india.nec.com>
2021-05-21 06:27:27 +0000

    remove fugfunction

--- application/operator/NGSI-LD-operator/WaterPumpFog/function.js
+++ application/operator/NGSI-LD-operator/WaterPumpFog/function.js
@@ -1,14 +1,23 @@
-var pof_observation = ""
+var current_observation = ""
+var previous_observation = ""
 var pon_observation = ""
 var onHasCreated = "false"
-var initiateUpdate = "false"
-var gotUpdate = "false"
+var publishStatus = "false"
 //var startTime = new Date().getTime();
 //  contextEntity: the received entities
 //  publish, query, and subscribe are the callback functions for your own function to interact with the assigned nearby broker
 //      publish:    publish the generated entity, which could be a new entity or the update of an existing entity
 //      query:      query addtional information from the assigned nearby broker
 //      subscribe:  subscribe addtional infromation from the assigned nearby broker
+
+function sleep(milliseconds) {
+  const date = Date.now();
+  let currentDate = null;
+  do {
+    currentDate = Date.now();
+  } while (currentDate - date < milliseconds);
+}
+
 
 exports.handler = function(contextEntity, publish, query, subscribe)
 {
@@ -26,47 +35,35 @@ exports.handler = function(contextEntity, publish, query, subscribe)
 	for (var key in contextEntity) {
 		updateEntity[key] = contextEntity[key]
 	}
-	console.log(updateEntity)
+
 	var con_observation = ""
-	var cof_observation = ""
+
 	if("on_status" in contextEntity) {
 		var con_observation = contextEntity.on_status.observedAt
 	}
-	if (con_observation != "" && onHasCreated == "false") {
-		value = "off"
-		onHasCreated = "true"
-		createDate = con_observation.split("T")
-		console.log("=====initial timer has been created======")
-	} else if (con_observation != "" && onHasCreated == "true") {
-		date = con_observation.split("T")
-		initiateUpdate = "true"
-		value  = "off"
-	}
-	if (initiateUpdate == "true") {
-		//console.log(date)
-		//console.log(createDate)
-		console.log(createDate[0])
-		console.log(date[0])
-		if (createDate[0] != date[0]) {
-			console.log("date is not equal")
-			updateEntity["command"] = {"type":"Property", "value": value}
-			onHasCreated = "false"
-			initiateUpdate = "false"
-			gotUpdate = "true"
-			publish(updateEntity)
-		 } else {
-			createH = createDate[1].split(":")
-			updateH = date[1].split(":")
-			console.log(parseInt(updateH[0]))
-			console.log(parseInt(createH[0]))
-			if (parseInt(updateH[0]) - parseInt(createH[0]) >= 1) {
-				updateEntity["command"] = {"type":"Property", "value": value}
-				publish(updateEntity)
-				onHasCreated = "false"
-				initiateUpdate = "false"
-				gotUpdate = "true"
-			}
+	current_observation = con_observation
+	if (current_observation != previous_observation) { 
+		previous_observation = current_observation
+		if (con_observation != "" && onHasCreated == "false") {
+			console.log("=====initial timer has been created======")
+			value = "off"
+			onHasCreated = "true"
+			createDate = con_observation.split("T")
+			publishStatus = "true"
 		}
+
+		if (publishStatus == "true") {
+			for(var i = 0 ; i<60 ;i++) {
+				console.log("Wait for publish")
+				sleep(1000);	
+			}
+			updateEntity["command"] = {"type":"Property", "value": value}
+			publish(updateEntity)
+			publishStatus = "false"
+			onHasCreated = "false"
+		}
+	} else {
+		console.log("Status is already off")
 	}
 	
  	// ============================== subscribe ======================================================   
