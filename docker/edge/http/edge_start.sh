fdb7011a972841f664f4e1615ed756745b665a62
pradumnnec <t_pradumn@india.nec.com>
2021-06-10 16:20:50 +0530

    Comment Incorporation

--- docker/edge/http/edge_start.sh
+++ docker/edge/http/edge_start.sh
@@ -1,3 +1,9 @@
+#This script is used to launch the docker containers for edge components i.e. Fogflow Edge Broker and Fogflow Edge Worker
+#The input to script decides which docker image is to be run in docker conatiners
+#For e.g. 
+# ./edge_start.sh : this command will run the docker image with "3.2.6" tag i.e. fogflow/broker:3.2.6
+# ./edge_start.sh arm : this command will run the docker image with "arm" tag  i.e. fogflow/broker:arm
+
 if [ $# -eq 0 ]; then
 	htype='3.2.6'
 else
--- docker/edge/http/edge_stop.sh
+++ docker/edge/http/edge_stop.sh
@@ -1,2 +1,4 @@
+#This script is used to stop and remove the docker containers for  edge components.
+
 docker stop edgebroker && docker rm $_
 docker stop edgeworker && docker rm $_
