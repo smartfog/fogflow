14fc5febde965d0a735a27f7f4083bb7bd2e70a4
showersky <showersky@gmail.com>
2022-05-06 16:59:15 +0200

    separate the interface implementation of the broker into different files for ngsiv1, v2, and ld

---
+++ broker/ngsild.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"github.com/ant0ine/go-json-rest/rest"
+)
+
+// ============= update ====================
+
+func (tb *ThinBroker) NGSILD_UpdateContext(w rest.ResponseWriter, r *rest.Request) {
+
+}
+
+func (tb *ThinBroker) NGSILD_DeleteEntity(w rest.ResponseWriter, r *rest.Request) {
+
+}
+
+func (tb *ThinBroker) NGSILD_DeleteAttribute(w rest.ResponseWriter, r *rest.Request) {
+
+}
+
+// ============= query ====================
+
+func (tb *ThinBroker) NGSILD_QueryByPostedFilters(w rest.ResponseWriter, r *rest.Request) {
+
+}
+
+func (tb *ThinBroker) NGSILD_QueryById(w rest.ResponseWriter, r *rest.Request) {
+
+}
+
+func (tb *ThinBroker) NGSILD_QueryByParameters(w rest.ResponseWriter, r *rest.Request) {
+
+}
+
+// ============= subscribe and notify ====================
+
+func (tb *ThinBroker) NGSILD_SubcribeContext(w rest.ResponseWriter, r *rest.Request) {
+
+}
+
+func (tb *ThinBroker) NGSILD_NotifyContext(w rest.ResponseWriter, r *rest.Request) {
+
+}
+
+func (tb *ThinBroker) NGSILD_UnsubscribeLDContext(w rest.ResponseWriter, r *rest.Request) {
+
+}
---
+++ broker/ngsiv2.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"github.com/ant0ine/go-json-rest/rest"
+)
+
+func (tb *ThinBroker) NGSIV2_queryEntities(w rest.ResponseWriter, r *rest.Request) {
+
+}
+
+func (tb *ThinBroker) NGSIV2_createEntities(w rest.ResponseWriter, r *rest.Request) {
+
+}
